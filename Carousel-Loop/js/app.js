f162d7c9998cee3e568bbad303156bc7a9c4c4d2
Bernese-Corgi <jnyng0621@gmail.com>
2021-02-18 21:41:27 +0900

    autoplay

--- Carousel-Loop/js/app.js
+++ Carousel-Loop/js/app.js
@@ -2,17 +2,23 @@ const SLIDE_WIDTH = '351px';
 let currentSlide = 1;
 const $carouselSlides = document.createElement('div');
 let duration = 500;
+let timerId = null;
+let autoplay = true;
 
 const renderCarousel = ($container, images) => {
   $carouselSlides.classList.add('carousel-slides');
   const $fragment = document.createDocumentFragment();
   const $prevBtn = document.createElement('button');
   const $nextBtn = document.createElement('button');
+  const $autoBtn = document.createElement('button');
   $prevBtn.classList.add('carousel-control', 'prev');
   $nextBtn.classList.add('carousel-control', 'next');
+  $autoBtn.classList.add('carousel-control', 'autoplay');
 
   $prevBtn.innerHTML = '&laquo;';
   $nextBtn.innerHTML = '&raquo;';
+  $autoBtn.innerHTML = 'stop';
+
 
   images.forEach((_, index) => {
     const $slide = document.createElement('img');
@@ -31,6 +37,7 @@ const renderCarousel = ($container, images) => {
   $container.appendChild($carouselSlides);
   $container.appendChild($prevBtn);
   $container.appendChild($nextBtn);
+  $container.appendChild($autoBtn);
 
   $carouselSlides.style.setProperty('--currentSlide', currentSlide);
 
@@ -39,6 +46,10 @@ const renderCarousel = ($container, images) => {
 };
 
 const prevSlide = total => {
+  if (!autoplay) {
+    clearInterval(timerId);
+    timerId = null;
+  }
   if (currentSlide >= 1) {
     $carouselSlides.style.setProperty('--duration', duration);
     $carouselSlides.style.setProperty('--currentSlide', --currentSlide);
@@ -51,15 +62,20 @@ const prevSlide = total => {
       $carouselSlides.style.setProperty('--duration', 0);
       $carouselSlides.style.setProperty('--currentSlide', currentSlide);
     });
-
   }
 };
 
 const nextSlide = total => {
+  if (!autoplay) {
+    clearInterval(timerId);
+    timerId = null;
+  }
+
   if (currentSlide <= total) {
     $carouselSlides.style.setProperty('--duration', duration);
     $carouselSlides.style.setProperty('--currentSlide', ++currentSlide);
   }
+
   if (currentSlide === total + 1) {
     $carouselSlides.addEventListener('transitionend', () => {
       if (currentSlide !== total + 1) return;
@@ -71,17 +87,35 @@ const nextSlide = total => {
   }
 };
 
+const autoSliding = (images) => {
+  if (autoplay) {
+    timerId = setInterval(() => nextSlide(images.length), 2000);
+    document.querySelector('.autoplay').innerHTML = 'stop';
+  }
+  else {
+    clearTimeout(timerId);
+    document.querySelector('.autoplay').innerHTML = 'play';
+
+  }
+};
 
 const carousel = ($container, images) => {
   renderCarousel($container, images);
+  autoSliding(images);
 
   $container.addEventListener('click', ({ target }) => {
     if (!target.classList.contains('carousel-control')) return;
     if (target.classList.contains('prev')) {
+      autoplay = false;
       prevSlide(images.length);
     }
     if (target.classList.contains('next')) {
+      autoplay = false;
       nextSlide(images.length);
+    }
+    if (target.classList.contains('autoplay')) {
+      autoplay = !autoplay;
+      autoSliding(images, target);
     }
   });
 };
