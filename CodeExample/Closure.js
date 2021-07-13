0d477cf0c46f3f7e5dccd7260b437223917031a5
Bernese-Corgi <jnyng0621@gmail.com>
2021-07-13 23:58:01 +0900

    JS code sample: 클로저의 활용-3 IIFE를 사용해 클로저 생성

--- CodeExample/Closure.js
+++ CodeExample/Closure.js
@@ -1,7 +1,8 @@
-/* ---------------------------------- */
-/* 상태 관리를 위한 closure 활용 예시       */
-/*  - 함수가 호출될 때마다 누적되는 카운터 함수 */
-/* ---------------------------------- */
+/* --------------------------------------------- */
+/* 상태 관리를 위한 closure 활용 예시                  */
+/*  - 함수가 호출될 때마다 누적되는 카운터 함수            */
+/*  - 1번부터 끝번까지 코드를 리팩토링하는 방식의 예시입니다. */
+/* --------------------------------------------- */
 
 /* 1. 전역 변수에서 관리 (클로저 x) */
 // 카운트 상태 변수
@@ -31,3 +32,20 @@ const increaseLocal = function () {
 console.log(increaseLocal()); // 1
 console.log(increaseLocal()); // 1
 console.log(increaseLocal()); // 1
+
+/* 3. IIFE로 클로저 생성 */
+// 카운트 상태 변경 함수
+const increaseIIFE = (function () {
+  // 카운트 상태 변수
+  let num = 0;
+
+  // 클로저
+  return function () {
+    // 카운트 상태를 1만큼 증가 시킨다.
+    return ++num;
+  };
+})();
+
+console.log(increaseIIFE()); // 1
+console.log(increaseIIFE()); // 2
+console.log(increaseIIFE()); // 3
