9c5bf985a28a902b5a846a504ea18f93688dcbd0
Bernese-Corgi <jnyng0621@gmail.com>
2021-07-14 00:01:59 +0900

    JS code sample: 클로저의 활용 4 - 객체 안의 메서드를 클로저로 생성

--- CodeExample/Closure.js
+++ CodeExample/Closure.js
@@ -53,3 +53,23 @@ const increaseIIFE = (function () {
 console.log(increaseIIFE()); // 1
 console.log(increaseIIFE()); // 2
 console.log(increaseIIFE()); // 3
+
+/* 4. 객체 안의 메서드를 클로저로 생성 */
+const counter = (function () {
+  // 카운트 상태 변수
+  let num = 0;
+
+  return {
+    increase() {
+      return ++num;
+    },
+    decrease() {
+      return num > 0 ? --num : 0;
+    },
+  };
+})();
+
+console.log(counter.increase()); // 1
+console.log(counter.increase()); // 2
+console.log(counter.decrease()); // 1
+console.log(counter.decrease()); // 0
