84fb694f4c499d19ee159cad87a4a90db431b7bf
Bernese-Corgi <jnyng0621@gmail.com>
2021-07-14 00:51:05 +0900

    JS code sample: 클로저의 활용-4.2 객체 안의 메서드를 클로저로 생성 - 생성자 함수

--- CodeExample/Closure.js
+++ CodeExample/Closure.js
@@ -54,7 +54,7 @@ console.log(increaseIIFE()); // 1
 console.log(increaseIIFE()); // 2
 console.log(increaseIIFE()); // 3
 
-/* 4. 객체 안의 메서드를 클로저로 생성 */
+/* 4-1. 객체 안의 메서드를 클로저로 생성 (객체 리터럴) */
 const counter = (function () {
   // 카운트 상태 변수
   let num = 0;
@@ -73,3 +73,30 @@ console.log(counter.increase()); // 1
 console.log(counter.increase()); // 2
 console.log(counter.decrease()); // 1
 console.log(counter.decrease()); // 0
+
+/* 4-2. 객체 안의 메서드를 클로저로 생성 (생성자 함수)  */
+const CounterConstructor = (function () {
+  // 카운트 상태 변수
+  let num = 0;
+
+  function Counter() {}
+
+  // 클로저
+  Counter.prototype.increase = function () {
+    return ++num;
+  };
+
+  // 클로저
+  Counter.prototype.decrease = function () {
+    return num > 0 ? --num : 0;
+  };
+
+  return Counter;
+})();
+
+const counterInstance = new CounterConstructor();
+
+console.log(counterInstance.increase()); // 1
+console.log(counterInstance.increase()); // 2
+console.log(counterInstance.decrease()); // 1
+console.log(counterInstance.decrease()); // 0
