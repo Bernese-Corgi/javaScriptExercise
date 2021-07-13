8f0f20b2b4ee6c4a95c8dda2621ef9fe54109326
Bernese-Corgi <jnyng0621@gmail.com>
2021-07-13 23:53:53 +0900

    JS code sample: 클로저의 활용-2 지역 변수에서 관리 (클로저 x)

--- CodeExample/Closure.js
+++ CodeExample/Closure.js
@@ -16,3 +16,18 @@ const increaseGlobal = function () {
 console.log(increaseGlobal()); // 1
 console.log(increaseGlobal()); // 2
 console.log(increaseGlobal()); // 3
+
+/* 2. 지역 변수에서 관리 (클로저 x) */
+// 카운트 상태 변경 함수
+const increaseLocal = function () {
+  // 카운트 상태 변수
+  let num = 0;
+
+  // 카운트 상태를 1만큼 증가 시킨다.
+  return ++num;
+};
+
+// 이전 상태를 유지하지 못한다.
+console.log(increaseLocal()); // 1
+console.log(increaseLocal()); // 1
+console.log(increaseLocal()); // 1
